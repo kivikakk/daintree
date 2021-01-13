4bdcf1cd64f1db66d75a7a0b324a7f6fb033eba5
Asherah Connor <ashe@kivikakk.ee>
2021-01-13 15:30:57 +1100

    wip

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 .PHONY: qemu mk-ovmf-vars mk-disk
 
-qemu: dainboot/dainboot.cdr
+qemu: dainboot/dainboot.cdr disk.dmg
 	qemu-system-aarch64 \
 		-accel hvf \
 		-m 512 \
@@ -8,7 +8,7 @@ qemu: dainboot/dainboot.cdr
 		-drive file=/opt/homebrew/share/qemu/edk2-aarch64-code.fd,if=pflash,format=raw,readonly=on \
 		-drive file=ovmf_vars.fd,if=pflash,format=raw \
 		-serial telnet::4444,server,nowait \
-		-drive if=none,file=disk.qcow2,format=qcow2,id=hd0 \
+		-drive if=none,file=disk.dmg,format=raw,id=hd0 \
 		-cdrom dainboot/dainboot.cdr \
 		-device virtio-blk-device,drive=hd0,serial="dummyserial" \
 		-device virtio-net-device,netdev=net0 \
@@ -21,7 +21,15 @@ qemu: dainboot/dainboot.cdr
 		-usb \
 		-monitor stdio
 
-dainboot/disk/EFI/BOOT/BOOTAA64.efi: dainboot/build.zig dainboot/src/*.zig
+os/zig-cache/bin/dainkrnl: os/build.zig os/version.zig os/src/*.zig
+	cd os && zig build
+
+disk.dmg: os/zig-cache/bin/dainkrnl
+	hdiutil attach -mountpoint target disk.dmg
+	cp os/zig-cache/bin/dainkrnl target
+	hdiutil detach target
+
+dainboot/disk/EFI/BOOT/BOOTAA64.efi: dainboot/build.zig dainboot/version.zig dainboot/src/*.zig
 	cd dainboot && zig build
 
 dainboot/dainboot.cdr: dainboot/disk/EFI/BOOT/BOOTAA64.efi
@@ -31,5 +39,5 @@ mk-ovmf-vars:
 	dd if=/dev/zero conv=sync bs=1m count=64 of=ovmf_vars.fd
 
 mk-disk:
-	qemu-img create -f qcow2 disk.qcow2 128M
+	hdiutil create -size 128m -layout GPTSPUD -fs "MS-DOS FAT32" -volname DAINDISK disk.dmg
 
--- dainboot/src/dainboot.zig
+++ dainboot/src/dainboot.zig
@@ -22,42 +22,48 @@ pub fn main() void {
 
     printf(buf[0..], "daintree bootloader ({s})\r\n", .{build_options.version});
 
+    var graphics: *uefi.protocols.GraphicsOutputProtocol = undefined;
+    if (uefi.Status.Success != boot_services.locateProtocol(&uefi.protocols.GraphicsOutputProtocol.guid, null, @ptrCast(*?*c_void, &graphics))) {
+        return;
+    }
+    var fb: [*]u8 = @intToPtr([*]u8, graphics.mode.frame_buffer_base);
+
     var memory_map: [*]uefi.tables.MemoryDescriptor = undefined;
     var memory_map_size: usize = 0;
     var memory_map_key: usize = undefined;
     var descriptor_size: usize = undefined;
     var descriptor_version: u32 = undefined;
-    while (uefi.Status.BufferTooSmall == boot_services.getMemoryMap(&memory_map_size, memory_map, &memory_map_key, &descriptor_size, &descriptor_version)) {
-        if (uefi.Status.Success != boot_services.allocatePool(uefi.tables.MemoryType.BootServicesData, memory_map_size, @ptrCast(*[*]align(8) u8, &memory_map))) {
+    while (boot_services.getMemoryMap(
+        &memory_map_size,
+        memory_map,
+        &memory_map_key,
+        &descriptor_size,
+        &descriptor_version,
+    ) == uefi.Status.BufferTooSmall) {
+        if (boot_services.allocatePool(
+            uefi.tables.MemoryType.BootServicesData,
+            memory_map_size,
+            @ptrCast(*[*]align(8) u8, &memory_map),
+        ) != uefi.Status.Success) {
+            puts("failed to allocatePool\r\n");
             return;
         }
     }
 
-    var i: usize = 0;
-    while (i < memory_map_size / descriptor_size) : (i += 1) {
-        // See the UEFI specification for more information on the attributes.
-        printf(buf[0..], "*** {:3} type={s:23} physical=0x{x:0>16} virtual=0x{x:0>16} pages={:16} uc={} wc={} wt={} wb={} uce={} wp={} rp={} xp={} nv={} more_reliable={} ro={} sp={} cpu_crypto={} memory_runtime={}\r\n", .{
-            i,
-            @tagName(memory_map[i].type),
-            memory_map[i].physical_start,
-            memory_map[i].virtual_start,
-            memory_map[i].number_of_pages,
-            @boolToInt(memory_map[i].attribute.uc),
-            @boolToInt(memory_map[i].attribute.wc),
-            @boolToInt(memory_map[i].attribute.wt),
-            @boolToInt(memory_map[i].attribute.wb),
-            @boolToInt(memory_map[i].attribute.uce),
-            @boolToInt(memory_map[i].attribute.wp),
-            @boolToInt(memory_map[i].attribute.rp),
-            @boolToInt(memory_map[i].attribute.xp),
-            @boolToInt(memory_map[i].attribute.nv),
-            @boolToInt(memory_map[i].attribute.more_reliable),
-            @boolToInt(memory_map[i].attribute.ro),
-            @boolToInt(memory_map[i].attribute.sp),
-            @boolToInt(memory_map[i].attribute.cpu_crypto),
-            @boolToInt(memory_map[i].attribute.memory_runtime),
-        });
+    if (uefi.Status.Success != boot_services.exitBootServices(uefi.handle, memory_map_key)) {
+        puts("failed to exitBootServices\r\n");
+        return;
     }
 
-    _ = uefi.system_table.boot_services.?.stall(5 * 1000 * 1000);
+    // We may still use the frame buffer!
+
+    // draw some colors
+    var j: u32 = 0;
+    while (j < 640 * 480 * 4) : (j += 4) {
+        fb[j] = @truncate(u8, @divTrunc(j, 256));
+        fb[j + 1] = @truncate(u8, @divTrunc(j, 1536));
+        fb[j + 2] = @truncate(u8, @divTrunc(j, 2560));
+    }
+
+    while (true) {}
 }
