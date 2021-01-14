16b9e1ef437aaef88c4d7ea0a5c29cad7be63760
Asherah Connor <ashe@kivikakk.ee>
2021-01-14 22:39:04 +1100

    wip, crashes qemu

--- dainboot/src/dainboot.zig
+++ dainboot/src/dainboot.zig
@@ -140,7 +140,116 @@ pub fn main() void {
     _ = boot_services.stall(5 * 1000 * 1000);
 }
 
+// ref Figure D5-15
+fn PageTableEntry_u64(pte: PageTableEntry) u64 {
+    return @as(u64, pte.valid) |
+        (@as(u64, @enumToInt(pte.type)) << 1) | (@as(u64, pte.lba.attr_indx) << 2) | (@as(u64, pte.lba.ns) << 5) | (@as(u64, pte.lba.ap) << 6) | (@as(u64, pte.lba.sh) << 8) | (@as(u64, pte.lba.af) << 10) | (@as(u64, pte.lba.ng) << 11) | (@as(u64, pte.oa) << 29) | (@as(u64, pte.uba.pxn) << 53) | (@as(u64, pte.uba.uxn) << 54);
+}
+
+const PageTableEntry = struct {
+    valid: u1 = 1,
+    type: enum(u1) {
+        block = 0,
+        table = 1,
+    } = .block,
+    lba: struct {
+        attr_indx: u3 = 0,
+        ns: u1 = 0,
+        ap: u2 = 0b10, // read-only, EL1
+        sh: u2 = 0b11, // inner shareable
+        af: u1 = 0b1, // access flag (?)
+        ng: u1 = 0, // ??
+    } = .{},
+    _res0a: u4 = 0, // OA[51:48]
+    _res0b: u1 = 0, // nT
+    _res0c: u12 = 0,
+    oa: u19, // OA[47:29]
+    _res0d: u4 = 0,
+    uba: packed struct {
+        contiguous: u1 = 0,
+        pxn: u1 = 0,
+        uxn: u1 = 1,
+        _resa: u4 = 0,
+        _res0b: u4 = 0,
+        _resb: u1 = 0,
+    } = .{},
+};
+
+fn TCR_EL1_u64(tcr: TCR_EL1) u64 {
+    return 0b0000000000000000000000000000010111111111010101100111010100010110;
+}
+
+const TCR_EL1 = packed struct {
+    t0sz: u6 = 22, // TTBR0_EL1 addresses 2**(64-22) = 4TB
+    _res0a: u1 = 0,
+    epd0: u1 = 0, // enable TTBR0_EL1 walks (set = 1 to *disable*)
+    irgn0: u2 = 0b01, // WB/WA
+    orgn0: u2 = 0b01, // WB/WA
+    sh0: u2 = 0b11, // inner-shareable
+    tg0: packed enum(u2) {
+        K4 = 0b00,
+        K16 = 0b10,
+        K64 = 0b01,
+    } = .K64,
+    t1sz: u6 = 22, // TTBR1_EL1 addresses 2**(64-22) = 4TB
+    a1: u1 = 1, // TTBR1 contains ASID (???)
+    epd1: u1 = 0, // enable TTBR1_EL1 walks (set = 1 to *disable*)
+    irgn1: u2 = 0b11, // WB/WA
+    orgn1: u2 = 0b11, // WB/WA
+    sh1: u2 = 0b11, // inner-shareable
+    tg1: packed enum(u2) { // granule size for TTBR1_EL1
+        K4 = 0b10,
+        K16 = 0b01,
+        K64 = 0b11,
+    } = .K64,
+    ips: packed enum(u3) {
+        B32 = 0b000,
+        B48 = 0b101,
+    } = .B48,
+    _res0b: u1 = 0,
+    _as: u1 = 0,
+    tbi0: u1 = 0, // top byte ignored
+    tbi1: u1 = 0,
+    _ha: u1 = 0,
+    _hd: u1 = 0,
+    _hpd0: u1 = 0,
+    _hpd1: u1 = 0,
+    _hwu: u8 = 0,
+    _tbid0: u1 = 0,
+    _tbid1: u1 = 0,
+    _nfd0: u1 = 0,
+    _nfd1: u1 = 0,
+    _res0c: u9 = 0,
+};
+
+comptime {
+    if (@bitSizeOf(PageTableEntry) != 64) {
+        // @compileLog("PageTableEntry misshapen; ", @bitSizeOf(PageTableEntry));
+    }
+    if (@sizeOf(PageTableEntry) != 8) {
+        // @compileLog("PageTableEntry misshapen; ", @sizeOf(PageTableEntry));
+    }
+
+    if (@bitSizeOf(TCR_EL1) != 64) {
+        @compileLog("TCR_EL1 misshapen; ", @bitSizeOf(TCR_EL1));
+    }
+}
+
 fn exitBootServices(dainkrnl: [*]u8, dainkrnl_size: u64, dainkrnl_elf: elf.Header) noreturn {
+    var buf: [256]u8 = undefined;
+
+    var page_table: [*]u64 align(0x1000) = undefined;
+    check("allocatePages", boot_services.allocatePages(.AllocateAnyPages, .LoaderData, 16, @ptrCast(*[*]align(4096) u8, &page_table)));
+    printf(buf[0..], "allocated 16x4KiB pages at 0x{x:0>16} for page table\r\n", .{page_table});
+    for (page_table[0..8192]) |*pte, i| {
+        pte.* = PageTableEntry_u64(.{
+            .oa = @truncate(u19, 2 + i), // 512MB increments, so we start at 1GiB (where QEMU maps in memory) and go up
+        });
+        if (i < 2) {
+            printf(buf[0..], "i{}: {x:0>16}\r\n", .{ i, pte.* });
+        }
+    }
+
     var memory_map: [*]uefi.tables.MemoryDescriptor = undefined;
     var memory_map_size: usize = 0;
     var memory_map_key: usize = undefined;
@@ -162,8 +271,6 @@ fn exitBootServices(dainkrnl: [*]u8, dainkrnl_size: u64, dainkrnl_elf: elf.Heade
             halt();
         }
     }
-
-    var buf: [256]u8 = undefined;
 
     for (memory_map[0 .. memory_map_size / descriptor_size]) |ptr, i| {
         printf(buf[0..], "{:3} {s:23} p=0x{x:0>16} size={:16}\r\n", .{ i, @tagName(ptr.type), ptr.physical_start, ptr.number_of_pages << 12 });
@@ -214,17 +321,19 @@ fn exitBootServices(dainkrnl: [*]u8, dainkrnl_size: u64, dainkrnl_elf: elf.Heade
         \\msr ttbr0_el1, x0
         \\msr ttbr1_el1, x1
         \\msr tcr_el1, x2
+        \\msr mair_el1, x3
         \\isb
         \\mrs x0, sctlr_el1
         \\orr x0, x0, #1
         \\msr sctlr_el1, x0
         \\isb
-        \\br x3
+        \\br x4
         :
         : [ttbr0_el1] "{x0}" (@as(u64, 0)),
-          [ttbr1_el1] "{x1}" (@as(u64, 0)),
-          [tcr_el1] "{x2}" (@as(u64, 0)),
-          [entry] "{x3}" (dainkrnl_elf.entry + 8)
+          [ttbr1_el1] "{x1}" (page_table),
+          [tcr_el1] "{x2}" (TCR_EL1_u64(.{})),
+          [mair_el1] "{x3}" (@as(u64, 0xFF)),
+          [entry] "{x4}" (dainkrnl_elf.entry + 8)
         : "memory"
     );
 
--- os/src/entry.zig
+++ os/src/entry.zig
@@ -1,6 +1,11 @@
 export fn daintree_start() void {
     while (true) {
         asm volatile ("nop");
+        asm volatile ("br x0"
+            :
+            : [x0] "{x0}" (@as(u64, 0x1234567812345678))
+            : "memory"
+        );
         asm volatile ("wfi");
     }
 }
