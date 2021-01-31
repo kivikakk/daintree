c554e7fac7911ab3cd3c844802424a75f3f2b31c
Asherah Connor <ashe@kivikakk.ee>
2021-01-31 11:04:25 +1100

    clean up scripts
    poor hack in debug.sh, not sure how to fix comprehensively

--- debug.sh
+++ debug.sh
@@ -1,1 +1,2 @@
-gdb os/zig-cache/bin/dainkrnl -ex 'directory os/src' -ex 'set arch aarch64' -ex 'target remote :1234'
+cd "$(dirname "$0")"
+gdb os/zig-cache/bin/dainkrnl -ex 'directory os/src' -ex 'directory os/src/console' -ex 'set arch aarch64' -ex 'target remote :1234'
--- objdump.sh
+++ objdump.sh
@@ -1,1 +1,2 @@
+cd "$(dirname "$0")"
 objdump -dSl --prefix=. --prefix-strip=4 os/zig-cache/bin/dainkrnl|less
