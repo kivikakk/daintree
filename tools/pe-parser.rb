fd268da3757cea1a5cbdd22cca674d98be516205
Asherah Connor <ashe@kivikakk.ee>
2021-02-09 12:24:36 +1100

    parse relocs ourselves

--- tools/pe-parser.rb
+++ tools/pe-parser.rb
@@ -1,5 +1,19 @@
 #!/usr/bin/env ruby
 
+def relo_decode(e)
+  relo_type = {
+    0 => :IMAGE_REL_BASED_ABSOLUTE,
+    1 => :IMAGE_REL_BASED_HIGH,
+    2 => :IMAGE_REL_BASED_LOW,
+    3 => :IMAGE_REL_BASED_HIGHLOW,
+    4 => :IMAGE_REL_BASED_HIGHADJ,
+    5 => :IMAGE_REL_BASED_ARM_MOV32,  # Assuming ARM.
+    10 => :IMAGE_REL_BASED_DIR64,
+  }[e >> 12]
+  raise "unknown relo_type #{e >> 12}" if !relo_type
+  [relo_type, e & 0xfff]
+end
+
 pe = File.open(ARGV[0], "rb", &:read)
 
 raise "no MZ" if pe[0...2] != "MZ"
@@ -18,15 +32,54 @@ if optional_header_size > 0
     puts "PE32 (unimpl)"
   when 0x20b
     puts "PE32+"
-    lvmaj, lvmin, code_size, init_data_size, uninit_data_size, entry_point_relative, code_base, data_base =
-      opth[2..-1].unpack('CCVVVVVV')
+    lvmaj, lvmin, code_size, init_data_size, uninit_data_size, entry_point_relative, code_base,
+      image_base, section_alignment, file_alignment, osvermaj, osvermin, imagevermaj, imagevermin,
+      subsysvermaj, subsysvermin, _, imgsize, hdrsize, _, subsys, _, _, _, _, _, _, dd_count =
+      opth[2..-1].unpack('CCVVVVVQ<VVvvvvvvVVVVvvQ<Q<Q<Q<VV')
     puts "linker version #{lvmaj}.#{lvmin}"
     puts "sizes:        code: #{code_size}"
     puts "         init data: #{init_data_size}"
     puts "       uninit data: #{uninit_data_size}"
     puts "entry point: 0x#{entry_point_relative.to_s 16}"
-    puts "bases: code: 0x#{code_base.to_s 16}"
-    puts "       data: 0x#{data_base.to_s 16}"
+    puts "bases:  code: 0x#{code_base.to_s 16}"
+    puts "       image: 0x#{image_base.to_s(16).rjust(16, "0")}"
+    puts "section align #{section_alignment}, file align #{file_alignment}"
+    puts "requires OS version #{osvermaj}.#{osvermin}"
+    puts "image version #{imagevermaj}.#{imagevermin}"
+    puts "subsystem version #{subsysvermaj}.#{subsysvermin}"
+    puts "  image size: #{imgsize}"
+    puts "headers size: #{hdrsize}"
+    puts "subsystem: #{subsys}#{subsys == 10 ? " (EFI application)" : ""}"
+    puts "data dictionary entries: #{dd_count}"
+    dd_entries = opth[112...112+dd_count*8].unpack('V*').each_slice(2).to_a
+
+    sections = Hash[pe[coff_offset+4+20+optional_header_size...coff_offset+4+20+optional_header_size+section_count*40].bytes.each_slice(40).map do |sect|
+      name, vsz, va, rawsz, praw, _, _, _, _, _  = sect.pack('C*').unpack('Z8VVVVVVvvV')
+      [name, {
+        vsz: vsz,
+        va: va,
+        rawsz: rawsz,
+        praw: praw,
+      }]
+    end]
+
+    if dd_entries[5]
+      va, sz = dd_entries[5]
+      reloc_data = pe[sections[".reloc"][:praw] - sections[".reloc"][:va] + va..-1][0...sz]
+      while reloc_data.length > 0
+        page_rva, block_size = reloc_data.unpack('VV')
+        entries = reloc_data[8...block_size].unpack('v*')
+        reloc_data = reloc_data[block_size..-1]
+        puts "Page RVA: 0x#{page_rva.to_s 16}"
+        entries.map! { |e| relo_decode(e) }
+        entries.select! { |relo_type, _| relo_type == :IMAGE_REL_BASED_DIR64 }
+        puts "#{entries.length} relocations:"
+        entries.each_slice(8) do |s|
+          print "  "
+          puts s.map { |_, offset| "0x#{(page_rva + offset).to_s 16}" }.join(" ")
+        end
+      end
+    end
   else
     raise "unknown optional header magic 0x#{magic.to_s 16}"
   end
