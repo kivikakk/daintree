7cd74737bb86a7dd0f5f3cae62e1d1f236e2f648
Asherah Connor <ashe@kivikakk.ee>
2021-02-12 14:50:58 +1100

    continue refactor

--- dainboot/src/dainboot.zig
+++ dainboot/src/dainboot.zig
@@ -3,41 +3,12 @@ const uefi = std.os.uefi;
 const build_options = @import("build_options");
 const elf = @import("elf.zig");
 
-var con_out: *uefi.protocols.SimpleTextOutputProtocol = undefined;
+usingnamespace @import("util.zig");
+
 var boot_services: *uefi.tables.BootServices = undefined;
 
-fn puts(msg: []const u8) void {
-    for (msg) |c| {
-        const c_ = [2]u16{ c, 0 };
-        _ = con_out.outputString(@ptrCast(*const [1:0]u16, &c_));
-    }
-}
-
-fn printf(comptime format: []const u8, args: anytype) void {
-    var buf: [256]u8 = undefined;
-    puts(std.fmt.bufPrint(buf[0..], format, args) catch unreachable);
-}
-
-fn haltMsg(comptime msg: []const u8) noreturn {
-    puts("halted: " ++ msg ++ "\r\n");
-    halt();
-}
-
-fn halt() noreturn {
-    asm volatile ("msr daifset, #15");
-    while (true) {
-        asm volatile ("wfi");
-    }
-}
-
-fn check(comptime method: []const u8, result: uefi.Status) void {
-    if (result != .Success) {
-        haltMsg(method ++ " failed");
-    }
-}
-
 pub fn panic(msg: []const u8, error_return_trace: ?*std.builtin.StackTrace) noreturn {
-    printf("panic: {s}\n", .{msg});
+    printf("panic: {s}\r\n", .{msg});
     asm volatile ("b .");
     unreachable;
 }
@@ -48,6 +19,148 @@ pub fn main() void {
 
     printf("daintree bootloader {s} on {s}\r\n", .{ build_options.version, build_options.board });
 
+    var load_context = LoadContext{};
+
+    if (getLoadOptions()) |options| {
+        load_context.handleOptions(options);
+    }
+
+    if (load_context.dtb == null or load_context.dainkrnl == null) {
+        load_context.searchFileSystems();
+    }
+
+    const dtb = load_context.dtb orelse {
+        printf("\r\nDTB not found\r\n", .{});
+        _ = boot_services.stall(5 * 1000 * 1000);
+        return;
+    };
+    const dainkrnl = load_context.dainkrnl orelse {
+        printf("\r\nDAINKRNL not found\r\n", .{});
+        _ = boot_services.stall(5 * 1000 * 1000);
+        return;
+    };
+
+    exitBootServices(dainkrnl, dtb);
+}
+
+const LoadContext = struct {
+    const Self = @This();
+
+    dtb: ?[]const u8 = null,
+    dainkrnl: ?[]const u8 = null,
+
+    fn handleOptions(self: *Self, options: []const u8) void {
+        var it = std.mem.tokenize(options, " ");
+
+        loop: while (it.next()) |opt_name| {
+            if (std.mem.eql(u8, opt_name, "dtb")) {
+                const loc = handleOptionsLoc("dtb", &it) orelse continue :loop;
+                printf("using dtb in ramdisk at 0x{x:0>16} ({} bytes)\r\n", .{ loc.offset, loc.len });
+                self.dtb = @intToPtr([*]u8, loc.offset)[0..loc.len];
+            } else if (std.mem.eql(u8, opt_name, "ramdisk")) {
+                const loc = handleOptionsLoc("ramdisk", &it) orelse continue :loop;
+                printf("using kernel in ramdisk at 0x{x:0>16} ({} bytes)\r\n", .{ loc.offset, loc.len });
+                self.dainkrnl = @intToPtr([*]u8, loc.offset)[0..loc.len];
+            } else {
+                printf("unknown option '{s}'\r\n", .{opt_name});
+            }
+        }
+    }
+
+    fn searchFileSystems(self: *Self) void {
+        var handle_list_size: usize = 0;
+        var handle_list: [*]uefi.Handle = undefined;
+        while (boot_services.locateHandle(
+            .ByProtocol,
+            &uefi.protocols.SimpleFileSystemProtocol.guid,
+            null,
+            &handle_list_size,
+            handle_list,
+        ) == .BufferTooSmall) {
+            check("allocatePool", boot_services.allocatePool(
+                uefi.tables.MemoryType.BootServicesData,
+                handle_list_size,
+                @ptrCast(*[*]align(8) u8, &handle_list),
+            ));
+        }
+
+        if (handle_list_size == 0) {
+            printf("no simple file system protocols found\r\n", .{});
+            return;
+        }
+
+        const handle_count = handle_list_size / @sizeOf(uefi.Handle);
+
+        printf("searching for <", .{});
+        if (self.dtb == null) {
+            printf("DTB", .{});
+            if (self.dainkrnl == null) printf(" ", .{});
+        }
+        if (self.dainkrnl == null) printf("DAINKRNL", .{});
+        printf("> on {} volume(s) ", .{handle_count});
+
+        for (handle_list[0..handle_count]) |handle| {
+            var sfs_proto: ?*uefi.protocols.SimpleFileSystemProtocol = undefined;
+
+            check("openProtocol", boot_services.openProtocol(
+                handle,
+                &uefi.protocols.SimpleFileSystemProtocol.guid,
+                @ptrCast(*?*c_void, &sfs_proto),
+                uefi.handle,
+                null,
+                .{ .get_protocol = true },
+            ));
+
+            printf(".", .{});
+
+            var f_proto: *uefi.protocols.FileProtocol = undefined;
+            check("openVolume", sfs_proto.?.openVolume(&f_proto));
+
+            if (self.dainkrnl == null) {
+                self.dainkrnl = tryLoadFromFileProtocol(f_proto, "dainkrnl");
+            }
+            if (self.dtb == null) {
+                self.dtb = tryLoadFromFileProtocol(f_proto, "dtb");
+            }
+
+            _ = boot_services.closeProtocol(handle, &uefi.protocols.SimpleFileSystemProtocol.guid, uefi.handle, null);
+
+            if (self.dainkrnl != null and self.dtb != null) {
+                break;
+            }
+        }
+    }
+
+    // ---
+
+    const Loc = struct {
+        offset: u64,
+        len: u64,
+    };
+
+    fn handleOptionsLoc(comptime opt_name: []const u8, it: *std.mem.TokenIterator) ?Loc {
+        const offset_s = it.next() orelse {
+            printf(opt_name ++ ": missing offset argument\r\n", .{});
+            return null;
+        };
+        const len_s = it.next() orelse {
+            printf(opt_name ++ ": missing length argument\r\n", .{});
+            return null;
+        };
+
+        const offset = std.fmt.parseInt(u64, offset_s, 0) catch |err| {
+            printf(opt_name ++ ": parse offset '{s}' error: {}\r\n", .{ offset_s, err });
+            return null;
+        };
+        const len = std.fmt.parseInt(u64, len_s, 0) catch |err| {
+            printf(opt_name ++ ": parse len '{s}' error: {}\r\n", .{ len_s, err });
+            return null;
+        };
+        return Loc{ .offset = offset, .len = len };
+    }
+};
+
+fn getLoadOptions() ?[]const u8 {
     var li_proto: ?*uefi.protocols.LoadedImageProtocol = undefined;
     if (boot_services.openProtocol(
         uefi.handle,
@@ -56,97 +169,27 @@ pub fn main() void {
         uefi.handle,
         null,
         .{ .get_protocol = true },
-    ) == .Success) {
-        var buffer: [256]u8 = [_]u8{undefined} ** 256;
-        const options_size = li_proto.?.load_options_size;
-        if (options_size > 0) {
-            var ptr: [*]u16 = @ptrCast([*]u16, @alignCast(@alignOf([*]u16), li_proto.?.load_options.?));
-            if (std.unicode.utf16leToUtf8(&buffer, ptr[0 .. options_size / 2])) |sz| {
-                var options = buffer[0..sz];
-                if (options.len > 0 and options[options.len - 1] == 0) {
-                    options = options[0 .. options.len - 1];
-                }
-                handleOptions(options);
-            } else |err| {
-                printf("failed utf16leToUtf8: {}\n", .{err});
-            }
-        }
+    ) != .Success) {
+        return null;
     }
 
-    // find traversable filesystems
-
-    var handle_list_size: usize = 0;
-    var handle_list: [*]uefi.Handle = undefined;
-    while (boot_services.locateHandle(
-        .ByProtocol,
-        &uefi.protocols.SimpleFileSystemProtocol.guid,
-        null,
-        &handle_list_size,
-        handle_list,
-    ) == .BufferTooSmall) {
-        check("allocatePool", boot_services.allocatePool(
-            uefi.tables.MemoryType.BootServicesData,
-            handle_list_size,
-            @ptrCast(*[*]align(8) u8, &handle_list),
-        ));
+    var buffer: [256]u8 = [_]u8{undefined} ** 256;
+    const options_size = li_proto.?.load_options_size;
+    if (options_size == 0) {
+        return null;
     }
 
-    if (handle_list_size == 0) {
-        haltMsg("no simple file system protocols found");
+    var ptr: [*]u16 = @ptrCast([*]u16, @alignCast(@alignOf([*]u16), li_proto.?.load_options.?));
+    if (std.unicode.utf16leToUtf8(&buffer, ptr[0 .. options_size / 2])) |sz| {
+        var options = buffer[0..sz];
+        if (options.len > 0 and options[options.len - 1] == 0) {
+            options = options[0 .. options.len - 1];
+        }
+        return options;
+    } else |err| {
+        printf("failed utf16leToUtf8: {}\r\n", .{err});
+        return null;
     }
-
-    const handle_count = handle_list_size / @sizeOf(uefi.Handle);
-
-    printf("searching for DAINKRNL and DTB on {} volume(s) ", .{handle_count});
-    var dainkrnl: ?[]const u8 = null;
-    var dainkrnl_elf: ?elf.Header = null;
-
-    var dtb: ?[]const u8 = null;
-
-    for (handle_list[0..handle_count]) |handle| {
-        var sfs_proto: ?*uefi.protocols.SimpleFileSystemProtocol = undefined;
-
-        check("openProtocol", boot_services.openProtocol(
-            handle,
-            &uefi.protocols.SimpleFileSystemProtocol.guid,
-            @ptrCast(*?*c_void, &sfs_proto),
-            uefi.handle,
-            null,
-            .{ .get_protocol = true },
-        ));
-
-        puts(".");
-
-        var f_proto: *uefi.protocols.FileProtocol = undefined;
-        check("openVolume", sfs_proto.?.openVolume(&f_proto));
-
-        if (dainkrnl == null) {
-            if (tryLoadFromFileProtocol(f_proto, "dainkrnl")) |bin| {
-                dainkrnl = bin;
-                dainkrnl_elf = parseElf(bin);
-            }
-        }
-        if (dtb == null) {
-            dtb = tryLoadFromFileProtocol(f_proto, "dtb");
-        }
-
-        _ = boot_services.closeProtocol(handle, &uefi.protocols.SimpleFileSystemProtocol.guid, uefi.handle, null);
-
-        if (dainkrnl != null and dtb != null) {
-            break;
-        }
-    }
-
-    if (dainkrnl_elf) |dainkrnl_elf_found| {
-        if (dtb) |dtb_found| {
-            exitBootServices(dainkrnl.?, dainkrnl_elf_found, dtb_found);
-        } else {
-            puts("\r\nDTB not found\r\n");
-        }
-    } else {
-        puts("\r\nDAINKRNL not found\r\n");
-    }
-    _ = boot_services.stall(5 * 1000 * 1000);
 }
 
 fn tryLoadFromFileProtocol(f_proto: *uefi.protocols.FileProtocol, comptime file_name: []const u8) ?[]const u8 {
@@ -181,92 +224,34 @@ fn tryLoadFromFileProtocol(f_proto: *uefi.protocols.FileProtocol, comptime file_
     return mem[0..size];
 }
 
-fn handleOptions(options: []const u8) void {
-    var it = std.mem.tokenize(options, " ");
-    var dtb: ?[]u8 = null;
-
-    while (it.next()) |opt_name| {
-        if (std.mem.eql(u8, opt_name, "dtb")) {
-            const loc = handleOptionsLoc("dtb", &it) orelse return;
-            printf("using dtb in ramdisk at 0x{x:0>16} ({} bytes)\r\n", .{ loc.offset, loc.len });
-            dtb = @intToPtr([*]u8, loc.offset)[0..loc.len];
-        } else if (std.mem.eql(u8, opt_name, "ramdisk")) {
-            const loc = handleOptionsLoc("ramdisk", &it) orelse return;
-
-            if (dtb == null) {
-                printf("can't load kernel in ramdisk without dtb\r\n", .{});
-                return;
-            }
-
-            printf("loading kernel in ramdisk at 0x{x:0>16} ({} bytes)\r\n", .{ loc.offset, loc.len });
-
-            const dainkrnl = @intToPtr([*]u8, loc.offset)[0..loc.len];
-            const dainkrnl_elf = parseElf(dainkrnl);
-            exitBootServices(dainkrnl, dainkrnl_elf, dtb.?);
-        } else {
-            printf("unknown option '{s}'\n", .{opt_name});
-        }
-    }
-}
-
-const Loc = struct {
-    offset: u64,
-    len: u64,
-};
-
-fn handleOptionsLoc(comptime opt_name: []const u8, it: *std.mem.TokenIterator) ?Loc {
-    const offset_s = it.next() orelse {
-        printf(opt_name ++ ": missing offset argument\n", .{});
-        return null;
-    };
-    const len_s = it.next() orelse {
-        printf(opt_name ++ ": missing length argument\n", .{});
-        return null;
-    };
-
-    if (it.next()) |unexp| {
-        printf(opt_name ++ ": unexpected argument '{s}'\n", .{unexp});
-        return null;
-    }
-
-    const offset = std.fmt.parseInt(u64, offset_s, 0) catch |err| {
-        printf(opt_name ++ ": parse offset '{s}' error: {}\n", .{ offset_s, err });
-        return null;
-    };
-    const len = std.fmt.parseInt(u64, len_s, 0) catch |err| {
-        printf(opt_name ++ ": parse len '{s}' error: {}\n", .{ len_s, err });
-        return null;
-    };
-    return Loc{ .offset = offset, .len = len };
-}
-
-fn parseElf(dainkrnl: []const u8) elf.Header {
-    if (dainkrnl.len < @sizeOf(elf.Elf64_Ehdr)) {
-        printf("found {} byte(s), too small for ELF header ({} bytes)\r\n", .{ dainkrnl.len, @sizeOf(elf.Elf64_Ehdr) });
+fn parseElf(bytes: []const u8) elf.Header {
+    if (bytes.len < @sizeOf(elf.Elf64_Ehdr)) {
+        printf("found {} byte(s), too small for ELF header ({} bytes)\r\n", .{ bytes.len, @sizeOf(elf.Elf64_Ehdr) });
         halt();
     }
 
-    var elf_buffer = std.io.fixedBufferStream(dainkrnl);
-    var dainkrnl_elf = elf.Header.read(&elf_buffer) catch |err| {
+    var buffer = std.io.fixedBufferStream(bytes);
+    var elf_header = elf.Header.read(&buffer) catch |err| {
         printf("failed to parse ELF: {}\r\n", .{err});
         halt();
     };
 
     printf("ELF entrypoint: {x:0>16} ({}-bit {c}E)\r\n", .{
-        dainkrnl_elf.entry,
-        @as(u8, if (dainkrnl_elf.is_64) 64 else 32),
-        @as(u8, if (dainkrnl_elf.endian == .Big) 'B' else 'L'),
+        elf_header.entry,
+        @as(u8, if (elf_header.is_64) 64 else 32),
+        @as(u8, if (elf_header.endian == .Big) 'B' else 'L'),
     });
 
-    var it = dainkrnl_elf.program_header_iterator(&elf_buffer);
+    var it = elf_header.program_header_iterator(&buffer);
     while (it.next() catch haltMsg("iterating phdr")) |phdr| {
         printf(" * type={x:0>8} off={x:0>16} vad={x:0>16} pad={x:0>16} fsz={x:0>16} msz={x:0>16}\r\n", .{ phdr.p_type, phdr.p_offset, phdr.p_vaddr, phdr.p_paddr, phdr.p_filesz, phdr.p_memsz });
     }
 
-    return dainkrnl_elf;
+    return elf_header;
 }
 
-fn exitBootServices(dainkrnl: []const u8, dainkrnl_elf: elf.Header, dtb: []const u8) noreturn {
+fn exitBootServices(dainkrnl: []const u8, dtb: []const u8) noreturn {
+    const dainkrnl_elf = parseElf(dainkrnl);
     var graphics: *uefi.protocols.GraphicsOutputProtocol = undefined;
     check("locateProtocol", boot_services.locateProtocol(&uefi.protocols.GraphicsOutputProtocol.guid, null, @ptrCast(*?*c_void, &graphics)));
     var fb: [*]u8 = @intToPtr([*]u8, graphics.mode.frame_buffer_base);
@@ -316,7 +301,7 @@ fn exitBootServices(dainkrnl: []const u8, dainkrnl_elf: elf.Header, dtb: []const
     const conventional_start = largest_conventional.?.physical_start;
     const conventional_bytes = largest_conventional.?.number_of_pages << 12;
 
-    printf("Using {}mb of memory starting at 0x{x:0>16}\n", .{ conventional_bytes >> 20, conventional_start });
+    printf("Using {}mb of memory starting at 0x{x:0>16}\r\n", .{ conventional_bytes >> 20, conventional_start });
 
     // The kernel's text section begins at 0xffffff80_00000000. Adjust those down
     // to conventional_start now.
