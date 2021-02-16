9d1b149617003bc23cf44686258988202a139b7a
Asherah Connor <ashe@kivikakk.ee>
2021-02-16 16:40:14 +1100

    try storing uart globally

--- os/src/hacks.zig
+++ os/src/hacks.zig
@@ -1,12 +1,14 @@
 const std = @import("std");
 const build_options = @import("build_options");
 
-const uart: u64 = if (comptime std.mem.eql(u8, build_options.board, "qemu"))
+pub var uart_global: ?*volatile u8 = null;
+
+const hack_uart_base: *volatile u8 = @intToPtr(*volatile u8, if (comptime std.mem.eql(u8, build_options.board, "qemu"))
     0x0900_0000
 else if (comptime std.mem.eql(u8, build_options.board, "rockpro64"))
     0xff1a_0000
 else
-    @compileError("hacks: unknown board");
+    @compileError("hacks: unknown board"));
 
 fn busyLoop() callconv(.Inline) void {
     var i: usize = 0;
@@ -16,33 +18,32 @@ fn busyLoop() callconv(.Inline) void {
 }
 
 pub fn HACK_uart(parts: anytype) callconv(.Inline) void {
-    HACK_uartAt(uart, parts);
+    HACK_uartAt(uart_global orelse hack_uart_base, parts);
 }
 
-pub fn HACK_uartAt(addr: u64, parts: anytype) callconv(.Inline) void {
-    const ptr = @intToPtr(*volatile u8, addr);
+pub fn HACK_uartAt(base: *volatile u8, parts: anytype) callconv(.Inline) void {
     const parts_info = std.meta.fields(@TypeOf(parts));
     comptime var i = 0;
     inline while (i < parts_info.len) : (i += 1) {
         if (comptime std.meta.trait.isPtrTo(.Array)(parts_info[i].field_type) or comptime std.meta.trait.isSliceOf(.Int)(parts_info[i].field_type)) {
-            HACK_uartWriteCarefully(ptr, parts[i]);
+            HACK_uartWriteCarefully(base, parts[i]);
         } else if (comptime std.meta.trait.isUnsignedInt(parts_info[i].field_type)) {
-            HACK_uartWriteCarefully(ptr, "0x");
-            HACK_uartWriteCarefullyHex(ptr, parts[i]);
+            HACK_uartWriteCarefully(base, "0x");
+            HACK_uartWriteCarefullyHex(base, parts[i]);
         } else {
             @compileError("what do I do with this? " ++ @typeName(parts_info[i].field_type));
         }
     }
 }
 
-fn HACK_uartWriteCarefully(ptr: *volatile u8, comptime msg: []const u8) callconv(.Inline) void {
+fn HACK_uartWriteCarefully(base: *volatile u8, comptime msg: []const u8) callconv(.Inline) void {
     inline for (msg) |c| {
-        ptr.* = c;
+        base.* = c;
         busyLoop();
     }
 }
 
-fn HACK_uartWriteCarefullyHex(ptr: *volatile u8, n: u64) callconv(.Inline) void {
+fn HACK_uartWriteCarefullyHex(base: *volatile u8, n: u64) callconv(.Inline) void {
     var digits: usize = 0;
     var c = n;
     while (c > 0) : (c /= 16) {
@@ -53,11 +54,11 @@ fn HACK_uartWriteCarefullyHex(ptr: *volatile u8, n: u64) callconv(.Inline) void 
     while (pow > 0) : (pow /= 16) {
         var digit = c / pow;
         if (digit >= 0 and digit <= 9) {
-            ptr.* = '0' + @truncate(u8, digit);
+            base.* = '0' + @truncate(u8, digit);
         } else if (digit >= 10 and digit <= 16) {
-            ptr.* = 'a' + @truncate(u8, digit) - 10;
+            base.* = 'a' + @truncate(u8, digit) - 10;
         } else {
-            ptr.* = '?';
+            base.* = '?';
         }
         busyLoop();
         c -= (digit * pow);
