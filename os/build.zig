a9e65f234f02231695f7646244279479306b7084
Asherah Connor <ashe@kivikakk.ee>
2021-02-17 16:29:03 +1100

    remove unused

--- os/build.zig
+++ os/build.zig
@@ -5,16 +5,11 @@ const Builder = std.build.Builder;
 const dbuild = @import("src/common/dbuild.zig");
 
 pub fn build(b: *Builder) !void {
-    // We used to force strict alignment since we run without MMU. Now we do enable it,
-    // but maybe we'll need to put it back on again later if our pre-MMU code generates a badly aligned access?
-    // var features = std.Target.Cpu.Feature.Set.empty;
-    // features.addFeature(@enumToInt(std.Target.aarch64.Feature.strict_align));
     const target = CrossTarget{
         .cpu_arch = .aarch64,
         .cpu_model = .{ .explicit = &std.Target.arm.cpu.cortex_a53 },
         .os_tag = .freestanding,
         .abi = .none,
-        // .cpu_features_add = features,
     };
 
     const board = try dbuild.getBoard(b);
