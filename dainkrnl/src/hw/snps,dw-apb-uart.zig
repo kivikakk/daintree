f4f07280015bcf46fbe82bb5e3c0f6aeb85f77dd
Asherah Connor <ashe@kivikakk.ee>
2021-03-17 16:24:36 +1100

    snps,dw-apb-uart: check LSR instead of busyLoop

--- dainkrnl/src/hw/snps,dw-apb-uart.zig
+++ dainkrnl/src/hw/snps,dw-apb-uart.zig
@@ -2,23 +2,19 @@
 // http://www.macs.hw.ac.uk/~hwloidl/hackspace/linux/drivers/tty/serial/8250/8250_dw.c
 // 8250 in the streets, 16750 in the sheets
 
-// hack!
-fn busyLoop() void {
-    var i: usize = 0;
-    while (i < 1_000) : (i += 1) {
-        asm volatile ("nop");
-    }
-}
-
 const REGOFF_RBR_THR = 0x00;
 const REGOFF_LSR = 0x05 << 2; // reg-shift
 const REGMASK_LSR_DATA_AVAIL = 1 << 0;
+const REGMASK_LSR_EMPTY_THR = 1 << 5;
 
 pub fn write(base: u64, data: []const u8) void {
     const thr = @intToPtr(*volatile u8, base + REGOFF_RBR_THR);
+    const lsr = @intToPtr(*volatile u8, base + REGOFF_LSR);
     for (data) |c| {
+        while (lsr.* & REGMASK_LSR_EMPTY_THR == 0) {
+            asm volatile ("nop");
+        }
         thr.* = c;
-        busyLoop();
     }
 }
 
